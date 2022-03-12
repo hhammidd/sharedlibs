ec4a705a63c406e44b37151c61ee9acab5ec311e
hamid <hamid.shafikani@gmail.com>
2022-03-12 14:01:05 +0100

    add build

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,19 +1,12 @@
 def call(version) {
-    stage("build Image") {
-        steps {
-            script {
-                dockerImage = docker.build registry + ":${version}"
-            }
-        }
+
+    script {
+        dockerImage = docker.build registry + ":${version}"
     }
 
-    stage("Push image") {
-        steps {
-            script {
-                docker.withRegistry('') {
-                    dockerImage.push()
-                }
-            }
+    script {
+        docker.withRegistry('') {
+            dockerImage.push()
         }
     }
 }
