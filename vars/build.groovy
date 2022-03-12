325434bea2e3c4940ceefce222122f050b2abcff
hamid <hamid.shafikani@gmail.com>
2022-03-12 13:48:08 +0100

    add build

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,31 +1,5 @@
 def call(service_name, VERSION) {
-    stage("git checkout") {
-        steps {
-            git 'https://github.com/hhammidd/${service_name}.git'
-        }
-    }
 
-
-    stage("build-test") {
-        steps {
-            sh "mvn clean install"
-        }
-    }
-    stage("build Image") {
-        steps {
-            script {
-                dockerImage = docker.build registry + ":${VERSION}"
-            }
-        }
-    }
-
-    stage("Push image") {
-        steps {
-            script {
-                docker.withRegistry('') {
-                    dockerImage.push()
-                }
-            }
-        }
-    }
+    git 'https://github.com/hhammidd/${service_name}.git'
+    sh "mvn clean install"
 }
