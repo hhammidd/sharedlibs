9d5868baf7de3779c0936b6b73b2e424f0eeef44
hamid <hamid.shafikani@gmail.com>
2022-03-14 00:19:25 +0100

    Add FE

--- vars/buildangularapp.groovy
+++ vars/buildangularapp.groovy
@@ -1,5 +1,6 @@
 def call(service_name, version) {
     git 'https://github.com/hhammidd/${service_name}.git'
 //    sh "git clone https://github.com/hhammidd/${service_name}.git"
+    sh "echo ${version}"
     sh "docker build -t ${service_name}:${version} ."
 }
--- vars/removeNoneImages.groovy
+++ vars/removeNoneImages.groovy
@@ -1,3 +1,4 @@
 def call() {
     sh "docker rmi \$(docker images --filter \"dangling=true\" -q --no-trunc)"
+    //docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 }
