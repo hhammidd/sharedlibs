f939336d03e269c2004e13b966567a502bf5c6a0
hamid <hamid.shafikani@gmail.com>
2022-03-30 23:13:48 +0200

    Add FE

--- vars/buildangularappold.groovy
+++
@@ -1,5 +0,0 @@
-def call(service_name, version) {
-    git 'https://github.com/hhammidd/${service_name}.git'
-//    sh "git clone https://github.com/hhammidd/${service_name}.git"
-    sh "docker build -t ${service_name}:${version}  . "
-}
