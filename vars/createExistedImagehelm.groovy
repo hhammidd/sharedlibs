8be1c93edde710ca22b927c24a2fbc6c6387f7ea
hamid <hamid.shafikani@gmail.com>
2022-03-31 19:58:22 +0200

    Add FE

--- vars/createExistedImagehelm.groovy
+++ vars/createExistedImagehelm.groovy
@@ -3,6 +3,11 @@ def call(image, version, environment) {
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/charts"
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/.git"
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
+    // checkout last Chart
+    sh "git clone https://github.com/hhammidd/Charts.git  ~/apps/apps-helm-charts/helm-checkouts/${image}/charts"
+
+    // replace spring boot helm.yml with value.yaml
+    sh "cp ~/apps/apps-helm-charts/helm-checkouts/${image}/code/helm.yml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/angular-apps/values.yaml"
 
     // start to deploy
     sh " helm upgrade --install ${image}  ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/angular-apps --set tag=${version} --namespace=${environment}"
