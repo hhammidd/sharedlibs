d8dbfb948de33a941c235a3ba2d9f2b57c34a763
hamid <hamid.shafikani@gmail.com>
2022-04-08 00:13:00 +0200

    Add owasp

---
+++ vars/startBuild.groovy
@@ -0,0 +1,6 @@
+def call(service_name, version) {
+    buildapp("${service_name}", {VERSION})
+    script {
+        currentBuild.description = "<b>environment: </b>${environment}<br/><b>version:</b>${VERSION}<br/><b>PR:</b>TODO"
+    }
+}
