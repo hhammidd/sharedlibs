bfbda38a1220bef8fcd693c66ee6015ffa8f7c42
hamid <hamid.shafikani@gmail.com>
2022-04-08 00:05:41 +0200

    Add owasp

--- vars/dos.groovy
+++ vars/dos.groovy
@@ -1,3 +1,11 @@
 def call(service_name, version) {
-    buildapp(service_name, version)
+    stage("start build process") {
+        steps {
+//                buildapp("${service_name}")
+            buildapp("${service_name}", "${VERSION}")
+            script {
+                currentBuild.description = "<b>environment: </b>${environment}<br/><b>version:</b>${VERSION}<br/><b>PR:</b>TODO"
+            }
+        }
+    }
 }
