d005ab3068b636c4b784ad19b43c7e688dea4cc2
hamid <hamid.shafikani@gmail.com>
2022-04-08 00:16:15 +0200

    Add owasp

---
+++ vars/buildimageProcess.groovy
@@ -0,0 +1,6 @@
+def call(version) {
+    buildimage(${version})
+    script {
+        currentBuild.description = "<b>environment: </b>${environment}<br/><b>version:</b>${VERSION}<br/><b>Image done:</b>${VERSION}"
+    }
+}
