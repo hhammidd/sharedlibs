7b644a0b97f48a5683c48e8d6dd27ac71240434e
hamid <hamid.shafikani@gmail.com>
2022-04-09 09:36:16 +0200

    Add angular

--- vars/createangularhelm.groovy
+++ vars/createangularhelm.groovy
@@ -10,9 +10,9 @@ def call(image, version, environment) {
     script {
         sh "cd  ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
         APP_VERSION1 = sh(script: 'node -e "console.log(require(\'./package.json\').version);"', returnStdout: true)
-        APP_VERSION = APP_VERSION1.toString()
+        APP_VERSION = "${APP_VERSION1}".toString()
         APP_VERSION = "0.0.0"
-        currentBuild.description = "<b>environment: </b>TODO}<br/><b>version:</b>${APP_VERSION}<br/><b>Image done:</b>TODO"
+        currentBuild.description = "<b>environment: </b>TODO<br/><b>version:</b>${APP_VERSION}<br/><b>Image done:</b>TODO"
     }
 
     // build image
