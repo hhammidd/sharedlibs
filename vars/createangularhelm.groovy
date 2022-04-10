aeda1ace9ac29ef2863e35cbc512677d297564e1
hamid <hamid.shafikani@gmail.com>
2022-04-10 13:37:43 +0200

    Add angular

--- vars/createangularhelm.groovy
+++ vars/createangularhelm.groovy
@@ -12,7 +12,7 @@ def call(image, version, environment) {
         APP_VERSION1 = sh(script: 'node -e "console.log(require(\'./package.json\').version);"', returnStdout: true)
         APP_VERSION = "${APP_VERSION1}".toString()
 //        APP_VERSION = "0.0.0"
-//        currentBuild.description = "<b>environment: </b>TODO<br/><b>version:</b>${APP_VERSION}<br/><b>Image done:</b>TODO"
+        currentBuild.description = "<b>environment: </b>TODO<br/><b>version:</b>${APP_VERSION}<br/><b>Image done:</b>TODO"
     }
 
 
