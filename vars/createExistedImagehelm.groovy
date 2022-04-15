06cde3f19680daf9aa81a76e4cb272ccb2307ea9
hamid <hamid.shafikani@gmail.com>
2022-04-16 00:42:56 +0200

    angular owasp

--- vars/createExistedImagehelm.groovy
+++ vars/createExistedImagehelm.groovy
@@ -4,6 +4,11 @@ def call(image, version, environment) {
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/.git"
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
 
+    // get the helm.yaml variables
+    sh "git clone https://github.com/hhammidd/${image}.git  ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
+
+    sh "cd  ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
+
     // checkout last Chart
     sh "git clone https://github.com/hhammidd/Charts.git  ~/apps/apps-helm-charts/helm-checkouts/${image}/charts"
 
