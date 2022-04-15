8a18e861572c5191db8a43e93a79b0ecb06e71f6
hamid <hamid.shafikani@gmail.com>
2022-04-16 00:36:11 +0200

    angular owasp

--- vars/createExistedImagehelm.groovy
+++ vars/createExistedImagehelm.groovy
@@ -3,24 +3,14 @@ def call(image, version, environment) {
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/charts"
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/.git"
     sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
-    // get the helm.yaml variables
-    sh "git clone https://github.com/hhammidd/${image}.git  ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
-
-
-
-    script {
-        sh "cd  ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
-        APP_VERSION = sh "node -e \"console.log(require('./package.json').version);\""
-        echo "${APP_VERSION}"
-    }
 
     // checkout last Chart
     sh "git clone https://github.com/hhammidd/Charts.git  ~/apps/apps-helm-charts/helm-checkouts/${image}/charts"
 
     // replace spring boot helm.yml with value.yaml
-
     sh "cp ~/apps/apps-helm-charts/helm-checkouts/${image}/code/helm.yml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/angular-apps/values.yaml"
 
-    // start to deploy
-    sh " helm upgrade --install ${image}  ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/angular-apps --set tag=${version} --namespace=${environment}"
+    sh " helm upgrade --install ${image}  ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/angular-apps --namespace=${environment} --set tag=${version} "
+
+    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
 }
