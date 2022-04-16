84c53ccf39f87ea6a200bad6cc3cd2913f3384fe
hamid <hamid.shafikani@gmail.com>
2022-04-16 14:20:06 +0200

    angular owasp

--- vars/bumpupVersion.groovy
+++ vars/bumpupVersion.groovy
@@ -1,9 +1,9 @@
-def call(image) {
+def call() {
     // make a new version
     sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit"
 
     // go to directory push it
     sh "cd ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
-    sh "git commit -m 'increament version to ${bla}' -- /root/apps/apps-helm-charts/helm-checkouts/${image}/code/"
+    sh "git commit -m 'increament version to ' -- /root/apps/apps-helm-charts/helm-checkouts/${image}/code/"
     sh "git push -u origin master"
 }
