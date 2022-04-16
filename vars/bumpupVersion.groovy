aa0b2b474b5f3c0610c75db6bf94518d971c31fe
hamid <hamid.shafikani@gmail.com>
2022-04-16 14:29:51 +0200

    angular owasp

--- vars/bumpupVersion.groovy
+++ vars/bumpupVersion.groovy
@@ -1,4 +1,4 @@
-def call(image) {
+def call(image, branch) {
     // make a new version
     sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit"
 
