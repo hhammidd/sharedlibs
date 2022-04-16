0467733ed1d63e35e2fc2e7da77c328cfa9ba848
hamid <hamid.shafikani@gmail.com>
2022-04-16 17:19:44 +0200

    angular owasp

--- vars/startBuild.groovy
+++ vars/startBuild.groovy
@@ -3,8 +3,6 @@ def call(service_name, version) {
 //    buildapp("${service_name}", "${version}", "${branch}")
     // TODO drop SNAPSHOT if it is PRD
     buildapp("${service_name}", "${version}")
-    sh 'cd ~/apps/apps-helm-charts/helm-checkouts/${service_name}/code'
-    sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion}\\-SNAPSHOT versions:commit" // TODO tetst
     script {
         currentBuild.description = "<b>branch: </b>${branch}<br/><b>environment: </b>${environment}<br/><b>version:</b>${VERSION}<br/><b>PR:</b>TODO"
     }
