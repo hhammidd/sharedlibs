4e007f24752622219c1ca7a3a35a57cdb6f2171d
hamid <hamid.shafikani@gmail.com>
2022-04-16 14:02:03 +0200

    angular owasp

--- vars/createhelm.groovy
+++ vars/createhelm.groovy
@@ -1,23 +1,22 @@
-def call(service_name, version) {
+def call(image) {
     // remove the dir
-    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts"
-    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/.git"
-    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${service_name}/code"
+    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/charts"
+    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/.git"
+    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
 
     // get the helm.yaml variables
-    sh "git clone https://github.com/hhammidd/${service_name}.git  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/code"
+    sh "git clone https://github.com/hhammidd/${image}.git  ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
 
     // checkout last Chart
-    sh "git clone https://github.com/hhammidd/Charts.git  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts"
+    sh "git clone https://github.com/hhammidd/Charts.git  ~/apps/apps-helm-charts/helm-checkouts/${image}/charts"
 
     // replace spring boot helm.yml with value.yaml
-    sh "cp ~/apps/apps-helm-charts/helm-checkouts/${service_name}/code/helm.yml ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-services/values.yaml"
+    sh "cp ~/apps/apps-helm-charts/helm-checkouts/${image}/code/helm.yml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/springboot-services/values.yaml"
 
     // cpy secrets to chart dir
-    sh "cp ~/apps/apps-helm-charts/secrets/${service_name}/secret.yaml ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-services/templates"
+    sh "cp ~/apps/apps-helm-charts/secrets/${image}/secret.yaml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/springboot-services/templates"
     // remove unwanted code
-    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${service_name}/code"
+    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
 
-    // start to deploy
     sh " helm upgrade --install ${service_name}  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-services --set tag=${version} --namespace=${environment}"
 }
