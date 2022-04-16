109f93efa753582a6ea570e72e5b8c9ad175d47c
hamid <hamid.shafikani@gmail.com>
2022-04-16 17:31:29 +0200

    angular owasp

--- vars/buildapp.groovy
+++ vars/buildapp.groovy
@@ -1,6 +1,13 @@
-def call(service_name, version) {
+def call(service_name, branch) {
 //    checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], userRemoteConfigs: [[url: 'git@git.servername:pathto/myrepo.git']]])
     git branch: '${branch}', url: 'https://github.com/hhammidd/${service_name}.git'
-    sh "mvn versions:set -DremoveSnapshot=true -DgenerateBackupPoms=false"
+
+    // if it  is env prd
+    script {
+        if ("${branch}"?.trim() == "prd") {
+            sh "mvn versions:set -DremoveSnapshot=true -DgenerateBackupPoms=false"
+        }
+    }
+
     sh "mvn clean install"
 }
