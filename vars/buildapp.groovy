e2c3c573208e39544a30fa35851f6db3f6072c3e
hamid <hamid.shafikani@gmail.com>
2022-04-16 13:40:35 +0200

    angular owasp

--- vars/buildapp.groovy
+++ vars/buildapp.groovy
@@ -1,4 +1,4 @@
-def call(service_name, version, branch) {
+def call(service_name, version) {
 //    checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], userRemoteConfigs: [[url: 'git@git.servername:pathto/myrepo.git']]])
     git branch: '${branch}', url: 'https://github.com/hhammidd/${service_name}.git'
     sh "mvn clean install"
--- vars/startBuild.groovy
+++ vars/startBuild.groovy
@@ -1,5 +1,7 @@
-def call(service_name, version, branch) {
-    buildapp("${service_name}", "${version}", "${branch}")
+def call(service_name, version) {
+//def call(service_name, version, branch) {
+//    buildapp("${service_name}", "${version}", "${branch}")
+    buildapp("${service_name}", "${version}")
     script {
         currentBuild.description = "<b>branch: </b>${branch}<br/><b>environment: </b>${environment}<br/><b>version:</b>${VERSION}<br/><b>PR:</b>TODO"
     }
