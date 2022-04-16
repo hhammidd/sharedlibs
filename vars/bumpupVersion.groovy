6f38adc5c338bd77f5981575b35dd1603227d659
hamid <hamid.shafikani@gmail.com>
2022-04-17 01:13:07 +0200

    angular owasp

--- vars/bumpupVersion.groovy
+++ vars/bumpupVersion.groovy
@@ -1,11 +1,18 @@
 def call(image, branch) {
-    // make a new version
-    script {
-        sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion}\\-SNAPSHOT versions:commit"
-    }
-
+    sh 'mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit'
     // go to directory push it
-    sh "cd ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
+    sh "cd ~/apps/apps-helm-charts/helm-checkouts/sale-point-service/code"
     sh "git commit -m 'increament version to ' -- /root/apps/apps-helm-charts/helm-checkouts/${image}/code/"
     sh "git push -u origin master"
+
+    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
+    // make a new version
+//    script {
+//        sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion}\\-SNAPSHOT versions:commit"
+//    }
+//
+//    // go to directory push it
+//    sh "cd ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
+//    sh "git commit -m 'increament version to ' -- /root/apps/apps-helm-charts/helm-checkouts/${image}/code/"
+//    sh "git push -u origin master"
 }
--- vars/createhelm.groovy
+++ vars/createhelm.groovy
@@ -16,17 +16,11 @@ def call(image, version) { // TODO get branch def call(image, branch) {
     // cpy secrets to chart dir
     sh "cp ~/apps/apps-helm-charts/secrets/${image}/secret.yaml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/springboot-services/templates"
     // remove unwanted code
-    sh "rm -rf ~/apps/apps-helm-charts/helm-checkouts/${image}/code"
     // TODO if the environment is tst, VERSION has SNAPSHOT, if deploy is PRD VERSION
     //
     sh " helm upgrade --install ${service_name}  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-services --set tag=${version} --namespace=${environment}"
 
     //TODO remove from below
     // make a new version
-//    sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit"
-    sh 'mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit'
-    // go to directory push it
-    sh "cd ~/apps/apps-helm-charts/helm-checkouts/sale-point-service/code"
-    sh "git commit -m 'increament version to ${bla}' -- /root/apps/apps-helm-charts/helm-checkouts/${image}/code/"
-    sh "git push -u origin master"
+
 }
