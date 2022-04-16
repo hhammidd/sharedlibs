1bd4489d336c66a4ea3b08e6d2a7b72c9bf95da6
hamid <hamid.shafikani@gmail.com>
2022-04-16 17:36:47 +0200

    angular owasp

--- vars/createhelm.groovy
+++ vars/createhelm.groovy
@@ -20,4 +20,13 @@ def call(image, version) { // TODO get branch def call(image, branch) {
     // TODO if the environment is tst, VERSION has SNAPSHOT, if deploy is PRD VERSION
     //
     sh " helm upgrade --install ${service_name}  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-services --set tag=${version} --namespace=${environment}"
+
+    //TODO remove from below
+    // make a new version
+    sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit"
+
+    // go to directory push it
+    sh "cd ~/apps/apps-helm-charts/helm-checkouts/sale-point-service/code"
+    sh "git commit -m 'increament version to ${bla}' -- /root/apps/apps-helm-charts/helm-checkouts/${image}/code/"
+    sh "git push -u origin master"
 }
