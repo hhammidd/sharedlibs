e1cfca63699b7ee08d8a1f06e30c5bb32ed215a0
hamid <hamid.shafikani@gmail.com>
2022-04-17 01:37:16 +0200

    angular owasp

--- vars/buildimageProcess.groovy
+++ vars/buildimageProcess.groovy
@@ -1,6 +1,6 @@
 def call(version) {
     buildimage("${version}")
     script {
-        currentBuild.description = "<b>environment: </b>${environment}<br/><b>version:</b>${VERSION}<br/><b>Image done:</b>${VERSION}"
+        currentBuild.description = "<b>branch: </b>${branch}<br/><b>environment: </b>${environment}<br/><b>version:</b>${version}<br/><b>PR:</b>TODO"
     }
 }
--- vars/startBuild.groovy
+++ vars/startBuild.groovy
@@ -1,7 +1,7 @@
 def call(service_name, version) {
     // TODO drop SNAPSHOT if it is PRD
     buildapp("${service_name}", "${environment}")
-    script {
-        currentBuild.description = "<b>branch: </b>${branch}<br/><b>environment: </b>${environment}<br/><b>version:</b>${VERSION}<br/><b>PR:</b>TODO"
-    }
+//    script {
+//        currentBuild.description = "<b>branch: </b>${branch}<br/><b>environment: </b>${environment}<br/><b>version:</b>${version}<br/><b>PR:</b>TODO"
+//    }
 }
