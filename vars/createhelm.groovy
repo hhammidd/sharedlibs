f8d6ef6570aa0884ad17cfe7fe1f5d5866e31f40
hamid <hamid.shafikani@gmail.com>
2022-04-17 01:07:46 +0200

    angular owasp

--- vars/createhelm.groovy
+++ vars/createhelm.groovy
@@ -23,8 +23,8 @@ def call(image, version) { // TODO get branch def call(image, branch) {
 
     //TODO remove from below
     // make a new version
-    sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit"
-
+//    sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit"
+    sh 'mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit'
     // go to directory push it
     sh "cd ~/apps/apps-helm-charts/helm-checkouts/sale-point-service/code"
     sh "git commit -m 'increament version to ${bla}' -- /root/apps/apps-helm-charts/helm-checkouts/${image}/code/"
