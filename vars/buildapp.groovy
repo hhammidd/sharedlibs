63e2d5d1f99617cd66d93c0b5d352788c265f094
hamid <hamid.shafikani@gmail.com>
2022-04-16 17:25:07 +0200

    angular owasp

--- vars/buildapp.groovy
+++ vars/buildapp.groovy
@@ -1,6 +1,6 @@
 def call(service_name, version) {
 //    checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], userRemoteConfigs: [[url: 'git@git.servername:pathto/myrepo.git']]])
     git branch: '${branch}', url: 'https://github.com/hhammidd/${service_name}.git'
-    sh "mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion}\\ versions:commit" // TODO tetst
+    sh "mvn versions:set -DremoveSnapshot=true -DgenerateBackupPoms=false"
     sh "mvn clean install"
 }
