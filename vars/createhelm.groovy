257fe61fb7c01a5939d9ef2ffd9f106527c35059
hamid <hamid.shafikani@gmail.com>
2022-04-17 11:50:03 +0200

    angular owasp

--- vars/createhelm.groovy
+++ vars/createhelm.groovy
@@ -22,12 +22,12 @@ def call(image, version) { // TODO get branch def call(image, branch) {
             sh " helm upgrade --install ${service_name}  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-services --set tag=${version} --namespace=${environment}"
         } else {
             // replace spring boot helm.yml with value.yaml
-            sh "cp ~/apps/apps-helm-charts/helm-checkouts/${image}/code/helm.yml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/springboot-services-api/springboot-services-tst/values.yaml"
+            sh "cp ~/apps/apps-helm-charts/helm-checkouts/${image}/code/helm.yml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/springboot-service-api/springboot-services-tst/values.yaml"
 
             // cpy secrets to chart dir
-            sh "cp ~/apps/apps-helm-charts/secrets/${image}/secret.yaml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/springboot-services-api/springboot-services-tst/templates"
+            sh "cp ~/apps/apps-helm-charts/secrets/${image}/secret.yaml ~/apps/apps-helm-charts/helm-checkouts/${image}/charts/springboot-service-api/springboot-services-tst/templates"
 
-            sh " helm upgrade --install ${service_name}  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-services-api/springboot-services-tst --set tag=${version} --namespace=${environment}"
+            sh " helm upgrade --install ${service_name}  ~/apps/apps-helm-charts/helm-checkouts/${service_name}/charts/springboot-service-api/springboot-services-tst --set tag=${version} --namespace=${environment}"
         }
     }
 
